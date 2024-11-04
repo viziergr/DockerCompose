2bbd1f42586e47b186978205d74308001130c2b8
viziergr <gregoire.vizier@reseau.eseo.fr>
2024-11-04 13:41:47 +0100

    Refactor Dockerfiles and docker-compose.yaml for improved configuration and defaults

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Construire l'image pour lemele-vizier.web
-docker compose build DockerfileLemeleVizier.web 
+docker compose build web
 
 # Construire l'image pour lemele-vizier.bdd
-docker compose build DockerfileLemeleVizier.bdd 
+docker compose build db
