d6c2ab97fb77378a1de9bfdcb0da52bf79b435c9
viziergr <gregoire.vizier@reseau.eseo.fr>
2024-11-04 11:50:47 +0100

    Simplify build script by removing unnecessary flags from docker compose commands

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Construire l'image pour lemele-vizier.web
-docker compose build -f DockerfileLemeleVizier.web -t lemele-vizier-web .
+docker compose build DockerfileLemeleVizier.web 
 
 # Construire l'image pour lemele-vizier.bdd
-docker compose build -f DockerfileLemeleVizier.bdd -t lemele-vizier-bdd .
+docker compose build DockerfileLemeleVizier.bdd 
