7d5caa8800acf9db7967ac1586262e495fe03753
viziergr <gregoire.vizier@reseau.eseo.fr>
2024-11-04 11:50:08 +0100

    Update build and run scripts to use 'docker compose' command for consistency

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Démarrage de Docker Compose
 echo "Démarrage de Docker Compose..."
-docker-compose up
+docker compose up
 
 # Option -d pour démarrer en détaché (en arrière-plan)
 # echo "Démarrage de Docker Compose en mode détaché..."
